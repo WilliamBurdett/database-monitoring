a47da54aed1c50b15c0c2ada39baf3019771abeb
William Burdett <william.burdett1@gmail.com>
2021-12-23 23:22:07 -0500

    Update alert.py

--- database_monitoring/orm/models/alert.py
+++ database_monitoring/orm/models/alert.py
@@ -4,3 +4,4 @@ from database_monitoring.orm.base_classes.model import Model
 class Alert(Model):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.columns.append(Column("inserted_timestamp", data_type="TIMESTAMP_NTZ", default="CURRENT_TIMESTAMP"))
